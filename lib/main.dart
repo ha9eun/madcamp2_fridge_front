d7c48b5736fc1a115250d53a32ce6940eed6d9f5
ha9eun <ha9eunsoo@gmail.com>
2024-07-05 23:59:49 +0900

    send user info to server

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Flutter와 네이티브 플랫폼 간의 상호작용
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -51,23 +52,52 @@ class _MyHomePageState extends State<MyHomePage> {
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
           print('카카오톡으로 로그인 성공 ${token.accessToken}');
+          await _getUserInfoAndSendToServer(token);
         } catch (error) {
           print('카카오톡으로 로그인 실패 $error');
           if (error is PlatformException && error.code == 'CANCELED') {
             return;
           }
-
         }
       } else {
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
           print('카카오계정으로 로그인 성공 ${token.accessToken}');
+          await _getUserInfoAndSendToServer(token);
         } catch (error) {
           print('카카오계정으로 로그인 실패 $error');
         }
       }
     } catch (error) {
       print('카카오톡 설치 여부 확인 실패 $error');
+    }
+  }
+
+  Future<void> _getUserInfoAndSendToServer(OAuthToken token) async {
+    try {
+      User user = await UserApi.instance.me();
+      await _sendUserInfoToServer(user, token);
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
+  }
+
+  Future<void> _sendUserInfoToServer(User user, OAuthToken token) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/kakaog/save_user/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'kakao_id': user.id.toString(),
+        'nickname': user.kakaoAccount.profile.nickname ?? '',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('사용자 정보 저장 성공');
+    } else {
+      print('사용자 정보 저장 실패');
     }
   }
 
