64787c178ba3281a322b0a4f86d78ad0f73c229c
ha9eun <ha9eunsoo@gmail.com>
2024-07-07 19:28:54 +0900

    feat/로그아웃 기능 다시

--- lib/view/my_page.dart
+++ lib/view/my_page.dart
@@ -22,6 +22,34 @@ class _MyPageState extends State<MyPage> {
     await ingredientViewModel.fetchIngredients(userViewModel.kakaoId);
   }
 
+  void _confirmLogout() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('로그아웃'),
+          content: Text('로그아웃 하시겠습니까?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+              },
+              child: Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                final userViewModel = Provider.of<UserViewModel>(context, listen: false);
+                userViewModel.logout(context);
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+              },
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final userViewModel = Provider.of<UserViewModel>(context);
@@ -32,10 +60,8 @@ class _MyPageState extends State<MyPage> {
         title: Text('${userViewModel.nickname} 님의 냉장고'),
         actions: [
           IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              // 설정 페이지로 이동
-            },
+            icon: Icon(Icons.logout),
+            onPressed: _confirmLogout,
           ),
         ],
       ),
@@ -68,15 +94,19 @@ class _MyPageState extends State<MyPage> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: ingredientViewModel.ingredients.length,
-              itemBuilder: (context, index) {
-                final ingredient = ingredientViewModel.ingredients[index];
-                return ListTile(
-                  title: Text(ingredient.foodName),
-                  subtitle: Text('양: ${ingredient.amount} ${ingredient.unit}, 유통기한: ${ingredient.expirationDate}'),
-                  onTap: () {
-                    // 상세 정보 보기
+            child: Consumer<IngredientViewModel>(
+              builder: (context, ingredientViewModel, child) {
+                return ListView.builder(
+                  itemCount: ingredientViewModel.ingredients.length,
+                  itemBuilder: (context, index) {
+                    final ingredient = ingredientViewModel.ingredients[index];
+                    return ListTile(
+                      title: Text(ingredient.foodName),
+                      subtitle: Text('양: ${ingredient.amount} ${ingredient.unit}, 유통기한: ${ingredient.expirationDate}'),
+                      onTap: () {
+                        // 상세 정보 보기
+                      },
+                    );
                   },
                 );
               },
